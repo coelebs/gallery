152e8f860329ea5a246b8be0de1ec700e2d1632c
vin <vin@kriek.org>
2018-05-11 22:48:08 +0200

    Improved parsing
    Moved models to seperate module.
    Added steps to parse only when data changed.

--- src/main.rs
+++ src/main.rs
@@ -1,193 +1,21 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
 
+extern crate image;
+extern crate libraw_sys as libraw;
+extern crate base64;
 extern crate quick_xml;
 extern crate rusqlite;
 extern crate rocket;
-extern crate libraw_sys as libraw;
-extern crate base64;
-extern crate image;
+extern crate time;
 
-use quick_xml::reader::Reader;
-use quick_xml::events::Event;
-use quick_xml::events::attributes::Attribute;
+pub mod model;
 
 use rusqlite::Connection;
 
-use std::io::BufReader;
 use std::fs;
 use std::path;
 use std::env;
-use std::ffi::CString;
-
-#[derive(Debug)]
-struct Image {
-    id: i64,
-    path: path::PathBuf,
-    rating: u8,
-    subjects: Vec<Subject>,
-}
-
-#[derive(Debug)]
-struct Subject {
-    id: i64,
-    family: String,
-    person: String,
-}
-
-impl Image {
-    fn initialize_db(conn: &Connection) {
-
-        conn.execute("DROP TABLE IF EXISTS Image;", &[]).unwrap();
-        conn.execute("CREATE TABLE Image (
-                        id          INTEGER PRIMARY KEY,
-                        path        TEXT,
-                        rating      INTEGER
-                      );", &[]).unwrap();
-
-        conn.execute("DROP TABLE IF EXISTS Image_Subjects;", &[]).unwrap();
-        conn.execute("CREATE TABLE Image_Subjects (
-                        img_subj_id INTEGER PRIMARY KEY, 
-                        image_id    INTEGER,
-                        subject_id  INGEGER
-                      );", &[]).unwrap();
-    }
-
-    fn insert(self, conn: &Connection) -> i64 {
-        conn.execute("INSERT INTO Image (path, rating)
-                      VALUES (?1, ?2)", &[&self.path.to_str(), &self.rating]).unwrap();
-
-        let image_id = conn.last_insert_rowid();
-
-        for mut subject in self.subjects {
-            subject.insert(conn);
-
-            conn.execute("INSERT INTO Image_Subjects (image_id, subject_id)
-                          VALUES (?1, ?2)", &[&image_id, &subject.id]).unwrap();
-        }
-
-        image_id
-    }
-
-    fn parse_rating(input: Vec<Attribute>, reader: &Reader<BufReader<fs::File>>) -> Option<u8> {
-        input.into_iter().filter(|x| x.key == b"xmp:Rating")
-              .map(|x| x.unescape_and_decode_value(reader).ok().unwrap().parse::<u8>().unwrap())
-              .last()
-    }
-
-    fn parse_xmp(path: &path::Path) -> Image {
-        let mut reader = 
-            Reader::from_file(path).ok().unwrap();
-
-        let mut rating = None;
-        let mut buf = Vec::new();
-        let mut subject = false;
-        let mut subjects = Vec::new();
-        loop {
-            match reader.read_event(&mut buf) {
-                Ok(Event::Start(ref e)) => 
-                    match e.name() {
-                        b"rdf:Description" => rating = Image::parse_rating(
-                                                            e.attributes()
-                                                            .map(|a| a.unwrap()).collect::<Vec<_>>(), &reader
-                                                            ),
-                        b"lr:hierarchicalSubject" => subject = true,
-                        _ => (),
-                    },
-                Ok(Event::End(ref e)) => 
-                    match e.name() {
-                        b"lr:hierarchicalSubject" => subject = false,
-                        _ => (),
-                    },
-                Ok(Event::Eof) => break,
-                Ok(Event::Text(ref e)) => if subject {subjects.push(e.unescape_and_decode(&reader).ok().unwrap())},
-                _ => (),
-            }
-
-            buf.clear();
-        }
-
-        subjects.retain(|x| x.trim().len() > 0);
-
-        Image {id: -1, path: path.to_path_buf(), rating: rating.unwrap(), subjects: Subject::parse_subjects(&subjects)}
-    }
-
-    fn base64_thumb(path: &path::Path) -> String {
-        let thumb_data;
-        unsafe {
-            let libraw_data = libraw::libraw_init(libraw::LIBRAW_OPTIONS_NONE);
-            
-            if libraw::libraw_open_file(libraw_data, 
-                                        CString::new(path.to_str().unwrap()).unwrap().as_ptr()) != 0 {
-                panic!("Libraw open file failed");
-            }
-
-            if libraw::libraw_unpack_thumb(libraw_data) != 0 {
-                panic!("Libraw unpack thumb failed");
-            }
-
-            /*if libraw::libraw_dcraw_thumb_writer(libraw_data, CString::new(output_fn).unwrap().as_ptr()) != 0 {
-                panic!("Libraw thumb_writer failed");
-            }*/
-            let mut result = 0;
-            let libraw_thumb = libraw::libraw_dcraw_make_mem_thumb(libraw_data, &mut result);
-            if result != 0 {
-                panic!("Libraw make mem thumb failed");
-            }
-
-            thumb_data = std::slice::from_raw_parts((*libraw_thumb).data.as_ptr(), 
-                                                    (*libraw_thumb).data_size as usize);
-        }
-
-        let mut img = image::load_from_memory(thumb_data).ok().unwrap();
-
-        img = img.thumbnail(1000, 1000);
-
-        let mut data = Vec::new();
-        img.write_to(&mut data, image::ImageFormat::JPEG);
-
-        base64::encode(&data)
-    }
-}
-
-impl Subject {
-    fn initialize_db(conn: &Connection) {
-        conn.execute("DROP TABLE IF EXISTS Subject;", &[]).unwrap();
-        conn.execute("CREATE TABLE Subject (
-                        id          INTEGER PRIMARY KEY,
-                        family      TEXT,
-                        person      TEXT
-                    );", &[]).unwrap();
-    }
-
-    fn insert(&mut self, conn: &Connection) {
-        let id = conn.query_row("SELECT id, family, person FROM Subject
-                                WHERE family = ?1 and person = ?2", 
-                                &[&self.family, &self.person], |x| { x.get(0)});
-
-        self.id = if id.is_ok() {
-            id.unwrap()
-        } else {
-            conn.execute("INSERT INTO Subject (family, person) 
-                          VALUES (?1, ?2)", &[&self.family, &self.person]).unwrap();
-
-            conn.last_insert_rowid()
-        };
-    }
-    
-    fn parse_subjects(input: &Vec<String>) -> Vec<Subject> {
-        let mut result = Vec::new();
-        for x in input {
-            let mut iter = x.rsplit("|");
-            let person = iter.next().unwrap();
-            let family = iter.next().unwrap();
-            result.push(Subject{id:-1, family: String::from(family), person: String::from(person)});
-        }
-
-        result
-    }
-
-}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -199,35 +27,19 @@ fn gallery(rating: u8) -> String {
     let mut result = String::from("<html><head/><body>");
 
     let conn = Connection::open("db.sqlite").ok().unwrap();
-    let mut qry = conn.prepare("SELECT id, rating, path FROM Image WHERE rating = ?1").unwrap();
+    let mut qry = conn.prepare("SELECT id, rating, path FROM model::Image WHERE rating = ?1").unwrap();
 
-    let image_iter = qry.query_map(&[&rating], |row| {
-         let path_s: String = row.get(2);
-         Image {
-            id: row.get(0),
-            path: path::PathBuf::from(path_s),
-            rating: row.get(1),
-            subjects: Vec::new()
-        }
-    }).unwrap();
+    let image_iter = qry.query_map(&[&rating], |row| model::Image::from_row(row)).unwrap();
     
-    let mut subj_qry = conn.prepare("SELECT s.id, s.family, person FROM Subject s
-                            LEFT JOIN Image_Subjects ims ON ims.subject_id = s.id
+    let mut subj_qry = conn.prepare("SELECT s.id, s.family, person FROM model::Subject s
+                            LEFT JOIN model::Image_model::Subjects ims ON ims.subject_id = s.id
                             WHERE ims.image_id = ?1").unwrap();
     for i in image_iter {
         let image = i.unwrap();
         result.push_str(&format!("{:?}\n", image.rating));
 
-        let raw_str = image.path.to_str().unwrap().replace(".xmp","");
-        let raw_path = path::Path::new(&raw_str);
-        let base64 = Image::base64_thumb(raw_path);
-        result.push_str("<img src=\"data:image/jpeg;base64,");
-        result.push_str(&base64);  
-        result.push_str("\"/>\n");
-        println!("didn't get to display it");
-
         let subject_iter = subj_qry.query_map(&[&image.id], |row| {
-            Subject {
+            model::Subject {
                 id: row.get(0),
                 family: row.get(1),
                 person: row.get(2),
@@ -247,22 +59,28 @@ fn gallery(rating: u8) -> String {
     result
 }
 
+fn read_dir(entry_point: &path::Path, conn: &rusqlite::Connection) {
+    for entry in fs::read_dir(entry_point).unwrap() {
+        let path = entry.ok().unwrap().path();
+        if path.is_file() && path.extension().unwrap().to_str() == Some("CR2") {
+            println!("Scanning {:?}", path);
+            model::Image::parse(&path, &conn).insert(&conn);
+        } else if path.is_dir() {
+            read_dir(&path, &conn);
+        }
+    }
+}
+
 fn main() {
     let args: Vec<_> = env::args().collect();
-    let entry_point = path::Path::new("/mnt/freenas/pictures/organised/2018/2018-04/2018-04-22/");
-
+    let entry_point = path::Path::new("/mnt/freenas/pictures/2018/");
 
     if args.len() > 1 {
         let conn = Connection::open("db.sqlite").ok().unwrap();
-        Image::initialize_db(&conn);
-        Subject::initialize_db(&conn);
+        model::Image::initialize_db(&conn);
+        model::Subject::initialize_db(&conn);
         
-        for entry in fs::read_dir(entry_point).unwrap() {
-            let path = entry.ok().unwrap().path();
-            if path.extension().unwrap().to_str() == Some("xmp") {
-                Image::parse_xmp(&path).insert(&conn);
-            }
-        }
+        read_dir(&entry_point, &conn);
     }
 
     rocket::ignite().mount("/", routes![index, gallery]).launch();
---
+++ src/model.rs
@@ -0,0 +1,237 @@
+use image;
+use libraw;
+use base64;
+use rusqlite;
+use std;
+use time;
+
+use quick_xml::reader::Reader;
+use quick_xml::events::Event;
+use quick_xml::events::attributes::Attribute;
+
+use std::io::BufReader;
+use std::fs;
+use std::path;
+use std::ffi::CString;
+
+#[derive(Debug)]
+pub struct Image {
+    pub id: i64,
+    pub path: path::PathBuf,
+    pub rating: u8,
+    pub subjects: Vec<Subject>,
+    pub last_modified: time::Timespec,
+}
+
+#[derive(Debug)]
+pub struct Subject {
+    pub id: i64,
+    pub family: String,
+    pub person: String,
+}
+
+impl Image {
+    pub fn initialize_db(conn: &rusqlite::Connection) {
+
+        conn.execute("CREATE TABLE IF NOT EXISTS Image (
+                        id              INTEGER PRIMARY KEY,
+                        path            TEXT,
+                        rating          INTEGER,
+                        last_modified   TEXT
+                      );", &[]).unwrap();
+
+        conn.execute("CREATE TABLE IF NOT EXISTS Image_Subjects (
+                        img_subj_id INTEGER PRIMARY KEY, 
+                        image_id    INTEGER,
+                        subject_id  INGEGER
+                      );", &[]).unwrap();
+    }
+
+    pub fn insert(self, conn: &rusqlite::Connection) -> i64 {
+        conn.execute("INSERT INTO Image (path, rating, last_modified)
+                      VALUES (?1, ?2, ?3)", 
+                      &[&self.path.to_str(), &self.rating, &self.last_modified])
+                    .unwrap();
+
+        let image_id = conn.last_insert_rowid();
+
+        for mut subject in self.subjects {
+            subject.insert(conn);
+
+            conn.execute("INSERT INTO Image_Subjects (image_id, subject_id)
+                          VALUES (?1, ?2)", &[&image_id, &subject.id]).unwrap();
+        }
+
+        image_id
+    }
+
+    pub fn from_row(row: &rusqlite::Row) -> Image {
+         Image {
+            id: row.get(0),
+            path: path::PathBuf::from(row.get::<i32, String>(1)),
+            rating: row.get(2),
+            subjects: Vec::new(),
+            last_modified: row.get(3)
+        }
+    }
+
+    fn parse_rating(input: Vec<Attribute>, reader: &Reader<BufReader<fs::File>>) -> Option<u8> {
+        input.into_iter().filter(|x| x.key == b"xmp:Rating")
+              .map(|x| x.unescape_and_decode_value(reader).ok().unwrap().parse::<u8>().unwrap())
+              .last()
+    }
+
+    fn parse_xmp(path: &path::Path) -> Image {
+        let xmp = path.with_extension(format!("{}.xmp", path.extension().unwrap()
+                                                            .to_str().unwrap()));
+
+        let mut reader = 
+            Reader::from_file(xmp).ok().unwrap();
+
+        let mut rating = None;
+        let mut buf = Vec::new();
+        let mut subject = false;
+        let mut subjects = Vec::new();
+        loop {
+            match reader.read_event(&mut buf) {
+                Ok(Event::Start(ref e)) => 
+                    match e.name() {
+                        b"rdf:Description" => rating = Image::parse_rating(
+                                                            e.attributes()
+                                                            .map(|a| a.unwrap()).collect::<Vec<_>>(), &reader
+                                                            ),
+                        b"lr:hierarchicalSubject" => subject = true,
+                        _ => (),
+                    },
+                Ok(Event::End(ref e)) => 
+                    match e.name() {
+                        b"lr:hierarchicalSubject" => subject = false,
+                        _ => (),
+                    },
+                Ok(Event::Eof) => break,
+                Ok(Event::Text(ref e)) => if subject {subjects.push(e.unescape_and_decode(&reader).ok().unwrap())},
+                _ => (),
+            }
+
+            buf.clear();
+        }
+
+        subjects.retain(|x| x.trim().len() > 0);
+
+        Image {
+            id: -1, 
+            path: path.to_path_buf(), 
+            rating: rating.unwrap(), 
+            subjects: Subject::parse_subjects(&subjects),
+            last_modified: time::now().to_timespec()
+        }
+    }
+
+    pub fn parse(path: &path::Path, conn: &rusqlite::Connection) -> Image {
+        let result;
+
+        let mut query = conn.prepare("SELECT * FROM Image
+                                      WHERE path = ?1").unwrap();
+        let mut image_iter = query.query_map(&[&path.to_path_buf().to_str()], |row| Image::from_row(row))
+                                  .unwrap();
+
+        let nxt = image_iter.next();
+        
+        if nxt.is_none() {
+            println!("\tImage new");
+            return Image::parse_xmp(path);
+        } 
+
+        let image = nxt.unwrap().unwrap();
+
+        if (image.last_modified.sec as u64) 
+              < path.metadata().unwrap().modified().unwrap()
+                    .duration_since(std::time::UNIX_EPOCH).unwrap() 
+                    .as_secs() {
+            println!("\tImage new or changed");
+            result = Image::parse_xmp(path); 
+        } else {
+            println!("\tImage unchanged");
+            result = image;
+        }
+
+        result
+    }
+
+    fn base64_thumb(path: &path::Path) -> String {
+        let thumb_data;
+        unsafe {
+            let libraw_data = libraw::libraw_init(libraw::LIBRAW_OPTIONS_NONE);
+            
+            if libraw::libraw_open_file(libraw_data, 
+                                        CString::new(path.to_str().unwrap()).unwrap().as_ptr()) != 0 {
+                panic!("Libraw open file failed");
+            }
+
+            if libraw::libraw_unpack_thumb(libraw_data) != 0 {
+                panic!("Libraw unpack thumb failed");
+            }
+
+            /*if libraw::libraw_dcraw_thumb_writer(libraw_data, CString::new(output_fn).unwrap().as_ptr()) != 0 {
+                panic!("Libraw thumb_writer failed");
+            }*/
+            let mut result = 0;
+            let libraw_thumb = libraw::libraw_dcraw_make_mem_thumb(libraw_data, &mut result);
+            if result != 0 {
+                panic!("Libraw make mem thumb failed");
+            }
+
+            thumb_data = std::slice::from_raw_parts((*libraw_thumb).data.as_ptr(), 
+                                                    (*libraw_thumb).data_size as usize);
+        }
+
+        let mut img = image::load_from_memory(thumb_data).ok().unwrap();
+
+        img = img.thumbnail(1000, 1000);
+
+        let mut data = Vec::new();
+        img.write_to(&mut data, image::ImageFormat::JPEG).unwrap();
+
+        base64::encode(&data)
+    }
+}
+
+impl Subject {
+    pub fn initialize_db(conn: &rusqlite::Connection) {
+        //conn.execute("DROP TABLE IF EXISTS Subject;", &[]).unwrap();
+        conn.execute("CREATE TABLE IF NOT EXISTS Subject (
+                        id          INTEGER PRIMARY KEY,
+                        family      TEXT,
+                        person      TEXT
+                    );", &[]).unwrap();
+    }
+
+    pub fn insert(&mut self, conn: &rusqlite::Connection) {
+        let id = conn.query_row("SELECT id, family, person FROM Subject
+                                WHERE family = ?1 and person = ?2", 
+                                &[&self.family, &self.person], |x| { x.get(0)});
+
+        self.id = if id.is_ok() {
+            id.unwrap()
+        } else {
+            conn.execute("INSERT INTO Subject (family, person) 
+                          VALUES (?1, ?2)", &[&self.family, &self.person]).unwrap();
+
+            conn.last_insert_rowid()
+        };
+    }
+    
+    fn parse_subjects(input: &Vec<String>) -> Vec<Subject> {
+        let mut result = Vec::new();
+        for x in input {
+            let mut iter = x.rsplit("|");
+            let person = iter.next().unwrap();
+            let family = iter.next().unwrap();
+            result.push(Subject{id:-1, family: String::from(family), person: String::from(person)});
+        }
+
+        result
+    }
+
+}
+
